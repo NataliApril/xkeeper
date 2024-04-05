9cbd7d33bb2509ce2c02b2a2346ba454c51f2098
Natali <natali.egorova4@yandex.ru>
2024-04-05 15:38:11 +0300

    new_wigets_QT

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication, 
-                             QHBoxLayout, QVBoxLayout, QGridLayout)
+                             QHBoxLayout, QVBoxLayout, QGridLayout, QLabel)
  
  
 class PyQtLayout(QWidget):
@@ -10,16 +10,34 @@ class PyQtLayout(QWidget):
         self.UI()
  
     def UI(self):
-        Button1 = QPushButton('PyQt')
-        Button2 = QPushButton('Layout')
-        Button3 = QPushButton('Management')
-         
-        vbox = QVBoxLayout()
-        vbox.addWidget(Button1)
-        vbox.addWidget(Button2)
-        vbox.addWidget(Button3)
+        '''speed control'''
+        Label1 = QLabel("Speed:")
+        Button_spd1 = QPushButton('+')
+        Button_spd2 = QPushButton('-')       
+        vbox_1 = QVBoxLayout()
+        vbox_1.addWidget(Label1)
+        vbox_1.addWidget(Button_spd1)
+        vbox_1.addWidget(Button_spd2)
+
+        '''direction control'''
+        Label2 = QLabel("Direction:")
+        Button_dir1 = QPushButton('+')
+        Button_dir2 = QPushButton('-')     
+        vbox_2 = QVBoxLayout()
+        vbox_2.addWidget(Label2)
+        vbox_2.addWidget(Button_dir1)
+        vbox_2.addWidget(Button_dir2)
+
+        '''steps control'''
+        Label3 = QLabel("Step:")
+        Button_step1 = QPushButton('+')
+        Button_step2 = QPushButton('-')     
+        vbox_3 = QVBoxLayout()
+        vbox_3.addWidget(Label3)
+        vbox_3.addWidget(Button_step1)
+        vbox_3.addWidget(Button_step2)
  
-        self.setLayout(vbox)
+        self.setLayout(vbox_1 | vbox_2 | vbox_3)
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('PyQt5 Layout')
         self.show()
