dd408ef2293e57914e67cc2aa7b448884882fa9f
NataliApril <natali.egorova4@yandex.ru>
2024-04-06 13:40:52 +0100

    test.py add can-communicate

--- test.py
+++ test.py
@@ -2,6 +2,15 @@ import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication, 
                              QHBoxLayout, QVBoxLayout, QGridLayout, 
                              QLabel, QGroupBox)
+import time 
+import can
+from gpiozero import LED
+import binascii
+
+interface = 'socketcan'
+channel = 'can0'
+
+led = LED(21)
  
  
 class PyQtLayout(QWidget):
@@ -41,6 +50,22 @@ class PyQtLayout(QWidget):
         self.show()
  
 
+def producer(id):
+    bus = can.Bus(channel=channel, interface=interface)
+    for i in range(10):
+        temp = binascii.unhexlify('4142434445464748')
+        msg = can.Message(arbitration_id=0xc0ffee,
+                          data=temp,
+                          is_extended_id=False)
+        bus.send(msg)
+        led.on()
+        time.sleep(1)     
+        led.off()
+        time.sleep(1)
+        print ("send", i)
+        
+    time.sleep(1)
+    
  
 if __name__ == '__main__':
     
