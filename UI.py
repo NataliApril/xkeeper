0aba474f252e38f749f0869c50aa71bc3b45d991
Natali <natali.egorova4@yandex.ru>
2024-04-09 16:10:49 +0300

    custom widget

--- UI.py
+++ UI.py
@@ -16,52 +16,33 @@ class App(QMainWindow):
         self.height = 800
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        
         self.table_widget = ServiceList(self)
         self.setCentralWidget(self.table_widget)
-        
-        # self.show()   
 
 class ServiceList(QWidget):
     
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
-        
-        # Initialize tab screen
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
         self.tab2 = QWidget()
         self.tabs.resize(300,200)
-
         self.TableServiceList()
-        
-        # Add tabs
         self.tabs.addTab(self.tab1,"Devices")
         self.tabs.addTab(self.tab2,"Service")
-        
-        # Create first tab
         self.tab2.layout = QVBoxLayout(self)
         self.tab2.layout.addWidget(self.ServiceList)
         self.tab2.setLayout(self.tab2.layout)
-        
-        # Add tabs to widget
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
-        
-    # @pyqtSlot()
-    # def on_click(self):
-    #     print("\n")
-    #     for currentQTableWidgetItem in self.tableWidget.selectedItems():
-    #         print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
 
+        
     def TableServiceList(self):
         self.ServiceList = QGroupBox()
-
         self.SpeedStatus()
         self.DirectionStatus()
         self.StepStatus()
-
         ServiceTab = QVBoxLayout()
         ServiceTab.addWidget(self.Step)
         ServiceTab.addWidget(self.Direction)
@@ -75,7 +56,6 @@ class ServiceList(QWidget):
         button_less = QPushButton("-")
         curr_val = QLabel(f'Current value: {100}')
         layout_spd = QHBoxLayout()
-        # layout_spd.setContentsMargins(5, 5, 5, 5)
         layout_spd.addWidget(speed)
         layout_spd.addWidget(button_less)
         layout_spd.addWidget(button_more)
@@ -89,7 +69,6 @@ class ServiceList(QWidget):
         button_CCV = QPushButton("CCV")
         curr_val_dir = QLabel(f'Current value: CV')
         layout_dir = QHBoxLayout()
-        # layout_dir.setContentsMargins(5, 5, 5, 5)
         layout_dir.addWidget(direction)
         layout_dir.addWidget(button_CCV)
         layout_dir.addWidget(button_CV)
@@ -103,205 +82,23 @@ class ServiceList(QWidget):
         button_stp_less = QPushButton("-")
         curr_stp_val = QLabel(f'Current value: {100}')
         layout_stp = QHBoxLayout()
-        # layout_stp.setContentsMargins(5, 5, 5, 5)
         layout_stp.addWidget(step)
         layout_stp.addWidget(button_stp_more)
         layout_stp.addWidget(button_stp_less)
         layout_stp.addWidget(curr_stp_val)
         self.Step.setLayout(layout_stp)
 
+    def Device (self, id, status):
+        print ("test")
 
 
-class DeviceCheak():
-    def __init__(self):
-        super().__init__()
-        self.vision()
+# class DeviceCheak(QWidget):
+    
+#     def __init__(self, parent):
+#         super(QWidget, self).__init__(parent)
+
+        
        
-
-  
-
-class WidgetGallery(QDialog):
-    def __init__(self, parent=None):
-        super(WidgetGallery, self).__init__(parent)
-
-        self.originalPalette = QApplication.palette()
-
-        styleComboBox = QComboBox()
-        styleComboBox.addItems(QStyleFactory.keys())
-
-        styleLabel = QLabel("&Style:")
-        # styleLabel.setBuddy(styleComboBox)
-
-        self.useStylePaletteCheckBox = QCheckBox("&Use style's standard palette")
-        self.useStylePaletteCheckBox.setChecked(True)
-
-        disableWidgetsCheckBox = QCheckBox("&Disable widgets")
-
-        self.createTopLeftGroupBox()
-        self.createTopRightGroupBox()
-        self.createBottomLeftTabWidget()
-        self.createBottomRightGroupBox()
-        '''self.createProgressBar()'''
-
-        # styleComboBox.textActivated.connect(self.changeStyle)
-        # self.useStylePaletteCheckBox.toggled.connect(self.changePalette)
-        disableWidgetsCheckBox.toggled.connect(self.topLeftGroupBox.setDisabled)
-        disableWidgetsCheckBox.toggled.connect(self.topRightGroupBox.setDisabled)
-        disableWidgetsCheckBox.toggled.connect(self.bottomLeftTabWidget.setDisabled)
-        disableWidgetsCheckBox.toggled.connect(self.bottomRightGroupBox.setDisabled)
-
-        topLayout = QHBoxLayout()
-        topLayout.addWidget(styleLabel)
-        topLayout.addWidget(styleComboBox)
-        topLayout.addStretch(1)
-        topLayout.addWidget(self.useStylePaletteCheckBox)
-        topLayout.addWidget(disableWidgetsCheckBox)
-
-        mainLayout = QGridLayout()
-        mainLayout.addLayout(topLayout, 0, 0, 1, 2)
-        mainLayout.addWidget(self.topLeftGroupBox, 1, 0)
-        mainLayout.addWidget(self.topLeftGroupBox, 1, 1)
-        mainLayout.addWidget(self.topLeftGroupBox, 2, 0)
-        mainLayout.addWidget(self.bottomRightGroupBox, 2, 1)
-        '''mainLayout.addWidget(self.progressBar, 3, 0, 1, 2)'''
-        mainLayout.setRowStretch(1, 1)
-        mainLayout.setRowStretch(2, 1)
-        mainLayout.setColumnStretch(0, 1)
-        mainLayout.setColumnStretch(1, 1)
-        self.setLayout(mainLayout)
-
-        self.setWindowTitle("Styles")
-        # self.changeStyle('Windows')
-
-    # def changeStyle(self, styleName):
-    #     QApplication.setStyle(QStyleFactory.create(styleName))
-    #     self.changePalette()
-
-    # def changePalette(self):
-    #     if (self.useStylePaletteCheckBox.isChecked()):
-    #         QApplication.setPalette(QApplication.style().standardPalette())
-    #     else:
-    #         QApplication.setPalette(self.originalPalette)
-
-    # def advanceProgressBar(self):
-    #     curVal = self.progressBar.value()
-    #     maxVal = self.progressBar.maximum()
-    #     self.progressBar.setValue(curVal + (maxVal - curVal) // 100)
-
-    def createTopLeftGroupBox(self):
-        self.topLeftGroupBox = QGroupBox("Group 1")
-
-        radioButton1 = QRadioButton("Radio button 1")
-        radioButton2 = QRadioButton("Radio button 2")
-        radioButton3 = QRadioButton("Radio button 3")
-        radioButton1.setChecked(True)
-
-        checkBox = QCheckBox("Tri-state check box")
-        checkBox.setTristate(True)
-        checkBox.setCheckState(Qt.CheckState.PartiallyChecked)
-
-        layout = QVBoxLayout()
-        layout.addWidget(radioButton1)
-        layout.addWidget(radioButton2)
-        layout.addWidget(radioButton3)
-        layout.addWidget(checkBox)
-        layout.addStretch(1)
-        self.topLeftGroupBox.setLayout(layout)
-
-    def createTopRightGroupBox(self):
-        self.topRightGroupBox = QGroupBox("Group 2")
-
-        defaultPushButton = QPushButton("Default Push Button")
-        defaultPushButton.setDefault(True)
-
-        togglePushButton = QPushButton("Toggle Push Button")
-        togglePushButton.setCheckable(True)
-        togglePushButton.setChecked(True)
-
-        flatPushButton = QPushButton("Flat Push Button")
-        flatPushButton.setFlat(True)
-
-        layout = QVBoxLayout()
-        layout.addWidget(defaultPushButton)
-        layout.addWidget(togglePushButton)
-        layout.addWidget(flatPushButton)
-        layout.addStretch(1)
-        self.topRightGroupBox.setLayout(layout)
-
-    def createBottomLeftTabWidget(self):
-        self.bottomLeftTabWidget = QTabWidget()
-        self.bottomLeftTabWidget.setSizePolicy(QSizePolicy.Policy.Preferred,
-                QSizePolicy.Policy.Ignored)
-
-        tab1 = QWidget()
-        tableWidget = QTableWidget(10, 10)
-
-        tab1hbox = QHBoxLayout()
-        tab1hbox.setContentsMargins(5, 5, 5, 5)
-        tab1hbox.addWidget(tableWidget)
-        tab1.setLayout(tab1hbox)
-
-        tab2 = QWidget()
-        textEdit = QTextEdit()
-
-        textEdit.setPlainText("Twinkle, twinkle, little star,\n"
-                              "How I wonder what you are.\n" 
-                              "Up above the world so high,\n"
-                              "Like a diamond in the sky.\n"
-                              "Twinkle, twinkle, little star,\n" 
-                              "How I wonder what you are!\n")
-
-        tab2hbox = QHBoxLayout()
-        tab2hbox.setContentsMargins(5, 5, 5, 5)
-        tab2hbox.addWidget(textEdit)
-        tab2.setLayout(tab2hbox)
-
-        self.bottomLeftTabWidget.addTab(tab1, "&Table")
-        self.bottomLeftTabWidget.addTab(tab2, "Text &Edit")
-
-    def createBottomRightGroupBox(self):
-        self.bottomRightGroupBox = QGroupBox("Group 3")
-        self.bottomRightGroupBox.setCheckable(True)
-        self.bottomRightGroupBox.setChecked(True)
-
-        # lineEdit = QLineEdit('s3cRe7')
-        # lineEdit.setEchoMode(QLineEdit.EchoMode.Password)
-
-        # spinBox = QSpinBox(self.bottomRightGroupBox)
-        # spinBox.setValue(50)
-
-        # dateTimeEdit = QDateTimeEdit(self.bottomRightGroupBox)
-        # dateTimeEdit.setDateTime(QDateTime.currentDateTime())
-
-        # slider = QSlider(Qt.Orientation.Horizontal, self.bottomRightGroupBox)
-        # slider.setValue(40)
-
-        # scrollBar = QScrollBar(Qt.Orientation.Horizontal, self.bottomRightGroupBox)
-        # scrollBar.setValue(60)
-
-        # dial = QDial(self.bottomRightGroupBox)
-        # dial.setValue(30)
-        # dial.setNotchesVisible(True)
-
-        # layout = QGridLayout()
-        # layout.addWidget(lineEdit, 0, 0, 1, 2)
-        # layout.addWidget(spinBox, 1, 0, 1, 2)
-        # layout.addWidget(dateTimeEdit, 2, 0, 1, 2)
-        # layout.addWidget(slider, 3, 0)
-        # layout.addWidget(scrollBar, 4, 0)
-        # layout.addWidget(dial, 3, 1, 2, 1)
-        # layout.setRowStretch(5, 1)
-        # self.bottomRightGroupBox.setLayout(layout)
-
-    '''def createProgressBar(self):
-        self.progressBar = QProgressBar()
-        self.progressBar.setRange(0, 10000)
-        self.progressBar.setValue(0)
-
-        timer = QTimer(self)
-        timer.timeout.connect(self.advanceProgressBar)
-        timer.start(1000)'''
-
 
 if __name__ == '__main__':
 
