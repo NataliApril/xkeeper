4297ee4d67a86e6b9545c41747677fd79e74c51d
Natali <natali.egorova4@yandex.ru>
2024-04-10 15:57:20 +0300

    test.py - raspberry testing programm

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication, 
-                             QHBoxLayout, QVBoxLayout, QGridLayout, QLabel)
+                             QHBoxLayout, QVBoxLayout, QGridLayout, 
+                             QLabel, QGroupBox)
  
  
 class PyQtLayout(QWidget):
@@ -14,38 +15,35 @@ class PyQtLayout(QWidget):
         Label1 = QLabel("Speed:")
         Button_spd1 = QPushButton('+')
         Button_spd2 = QPushButton('-')       
-        vbox_1 = QVBoxLayout()
-        vbox_1.addWidget(Label1)
-        vbox_1.addWidget(Button_spd1)
-        vbox_1.addWidget(Button_spd2)
 
         '''direction control'''
         Label2 = QLabel("Direction:")
-        Button_dir1 = QPushButton('+')
-        Button_dir2 = QPushButton('-')     
-        vbox_2 = QVBoxLayout()
-        vbox_2.addWidget(Label2)
-        vbox_2.addWidget(Button_dir1)
-        vbox_2.addWidget(Button_dir2)
+        Button_dir1 = QPushButton('CV')
+        Button_dir2 = QPushButton('CCV')     
 
         '''steps control'''
         Label3 = QLabel("Step:")
         Button_step1 = QPushButton('+')
         Button_step2 = QPushButton('-')     
-        vbox_3 = QVBoxLayout()
-        vbox_3.addWidget(Label3)
-        vbox_3.addWidget(Button_step1)
-        vbox_3.addWidget(Button_step2)
- 
-        self.setLayout(vbox_1 | vbox_2 | vbox_3)
+        widget = QGridLayout()
+        widget.addWidget(Label1, 0, 0)
+        widget.addWidget(Button_spd1, 0, 1)
+        widget.addWidget(Button_spd2, 0, 2)
+        widget.addWidget(Label2, 1, 0)
+        widget.addWidget(Button_dir1, 1, 1)
+        widget.addWidget(Button_dir2, 1, 2)
+        widget.addWidget(Label3, 2, 0)
+        widget.addWidget(Button_step1, 2, 1)
+        widget.addWidget(Button_step2, 2, 2)
+        self.setLayout(widget)
         self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('PyQt5 Layout')
+        self.setWindowTitle('Device control:')
         self.show()
  
-def main():
+
+ 
+if __name__ == '__main__':
+    
     app = QApplication(sys.argv)
     ex = PyQtLayout()
     sys.exit(app.exec_())
- 
-if __name__ == '__main__':
-    main()
