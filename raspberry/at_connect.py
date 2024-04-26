09cdc3fbeebe23c580d283eff94d180c3c995853
NataliApril <natali.egorova4@yandex.ru>
2024-04-26 12:08:50 +0100

    new version ui

--- raspberry/at_connect.py
+++ raspberry/at_connect.py
@@ -2,13 +2,15 @@ import serial
 import time
 
 serial_port = serial.Serial('/dev/ttyCH9344USB0', '115200')
-#serial_port.open()
 command = "ATI+CGSN\r\n"
 
-while True:
-	serial_port.write(command.encode())
-	response = serial_port.readline()
-	print (response)
-	time.sleep(1)
-	
-serial_port.close()
+def at_read_write():
+	while True:
+		serial_port.write(command.encode())
+		response = serial_port.readline()
+		print (response)
+		time.sleep(1)
+	serial_port.close()
+
+'''if __name__ == "__main__":
+	at_read_write()'''
--- raspberry/test_designer.py
+++ raspberry/test_designer.py
@@ -4,6 +4,7 @@ import sys
 import queue 
 import data_take as dt
 from threading import *
+import at_connect as at
 
 q = queue.Queue()
 
@@ -19,13 +20,19 @@ def data_in():
     while True:
         #dt.take()
         dt.put_data(q)
+        
+def at_con():
+    at.at_read_write()
     
         
 if __name__ == "__main__":
     t1 = Thread(target = UI)
     t2 = Thread(target = data_in)
+    t3 = Thread(target = at_con)
     t1.deamon = True
     t2.deamon = True
+    t3.deamon = True
     t1.start()
     t2.start()
+    t3.start()
     
